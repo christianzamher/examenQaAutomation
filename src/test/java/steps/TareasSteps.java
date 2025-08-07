60b6dec09495927318905bc08338a5d06da4eb65
ETI <czh1983@gmail.com>
2025-08-07 01:25:51 -0300

    agregado de logs a TareasSteps

--- src/test/java/steps/TareasSteps.java
+++ src/test/java/steps/TareasSteps.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.Hooks;
+import utilities.Logs;
 
 import java.time.Duration;
 import java.util.Map;
@@ -25,11 +26,13 @@ public class TareasSteps {
 
     @Given("El usuario está en la página principal")
     public void elUsuarioEstaEnLaPaginaPrincipal() {
+        Logs.info("Iniciando las pruebas");
         driver.get("http://93.188.165.79:8003");
     }
 
     @When("El usuario completa los campos {string}, {string} y {string}")
     public void elUsuarioCompletaLosCampos(String campo1, String campo2, String campo3, DataTable dataTable) {
+        Logs.info("Colocando info en los campos");
         Map<String, String> data = dataTable.asMaps().get(0);
 
         WebElement nombreInput = driver.findElement(By.id("task_name"));
@@ -43,12 +46,14 @@ public class TareasSteps {
 
     @When("El usuario hace clic en el botón {string}")
     public void elUsuarioHaceClicEnElBoton(String nombreBoton) {
+        Logs.info("Click en el boton");
         WebElement crearBoton = driver.findElement(By.xpath("//button[text()='" + nombreBoton + "']"));
         crearBoton.click();
     }
 
     @Then("La tarea {string} debería aparecer en la lista de tareas")
     public void laTareaDeberiaAparecerEnLaListaDeTareas(String nombreTarea) {
+        Logs.info("validacion de tareas en el gestor");
         // Define el localizador del elemento que vas a buscar
         By locator = By.xpath("//h5[contains(text(), 'Nombre Tarea: " + nombreTarea + "')]");
 
